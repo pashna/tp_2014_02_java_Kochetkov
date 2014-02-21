e2e5b81a5ad1f5e5c396015ac6ba60df49bc7e8c
pashna <p02p@yandex.ru>
2014-02-21 14:05:24 +0400

    HomeTask2

---
+++ src/main/java/frontend/FrontendFroSession.java
@@ -0,0 +1,74 @@
+package frontend;
+
+import templater.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class FrontendFroSession extends HttpServlet {
+
+    private String login = "";
+    private String pass = "";
+    private AtomicLong userIdGenerator = new AtomicLong();
+
+    public static String getTime() {
+        Date date = new Date();
+        date.getTime();
+        DateFormat formatter = new SimpleDateFormat("HH.mm.ss");
+        return formatter.format(date);
+    }
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        Map<String, Object> pageVariables = new HashMap<>();
+        if (request.getPathInfo().equals("/timer")) {
+            HttpSession session = request.getSession();
+            Long userId = (Long) session.getAttribute("userId");
+            if (userId == null) {
+                response.sendRedirect("/");
+            }
+            pageVariables.put("refreshPeriod", "1000");
+            pageVariables.put("serverTime", getTime());
+            pageVariables.put("userId", userId);
+            response.getWriter().println(PageGenerator.getPage("timer.tml", pageVariables));
+            return;
+        }
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        login = request.getParameter("login");
+        pass = request.getParameter("pass");
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        Map<String, Object> pageVariables = new HashMap<>();
+        if ((login.equals("pasha"))&&(pass.equals("123"))) {
+            HttpSession session = request.getSession();
+            if( !session.isNew() ) {
+                session.invalidate();
+                session = request.getSession();
+            }
+            Long userId = (Long) session.getAttribute("userId");
+            if (userId == null) {
+                userId = userIdGenerator.getAndIncrement();
+                session.setAttribute("userId", userId);
+            }
+            response.sendRedirect("/timer");
+        }
+        else {
+            response.sendRedirect("/");
+        }
+    }
+}
