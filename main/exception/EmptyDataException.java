8baa76cb90dbd7a1a422d4da4cfa77c0c25031f0
pashna <p02p@yandex.ru>
2014-04-20 05:30:39 +0400

    Revert "Revert "with one Test and start MS""
    This reverts commit f9768041692b7f78a829f5870ea1e88bedb42e32.

---
+++ main/exception/EmptyDataException.java
@@ -0,0 +1,11 @@
+package exception;
+
+/*
+ Created by p.Kochetkov on 29.03.14.
+ */
+
+public class EmptyDataException extends Exception{
+    public EmptyDataException(String s) {
+        super(s);
+    }
+}
